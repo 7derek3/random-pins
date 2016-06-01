5d6f72bd667069140915fb3de90026b8440691ba
Derek Miller <7derek3@gmail.com>
2016-06-01 11:12:16 -0700

    add function for getting random lat and lon

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+/* Get a random latitude and longitude somewhere in the United States */
+function randLatLon() {
+  var latRange = [18.005611, 48.987386]
+  var lonRange = [-124.626080, -62.361014]
+  var randLat  = Math.random() * (latRange[1] - latRange[0]) + latRange[0];
+  var randLon  = Math.random() * (lonRange[1] - lonRange[0]) + lonRange[0];
+  return [randLat, randLon]
+}
