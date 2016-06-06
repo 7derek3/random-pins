2b29f6ba4820d5ca5176b70dc42327eefff18b0c
Derek Miller <7derek3@gmail.com>
2016-06-06 14:28:00 -0700

    deal with machine pluralization and refactor to dry up code

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,10 +9,10 @@ function randLatLon() {
 
 /* Call random lat/lon function and create URI to call API */
 function getUri() {
-  var latLon = randLatLon();
+  var latLon  = randLatLon();
   var baseUri = "http://pinballmap.com/api/v1/locations/closest_by_lat_lon.json";
-  var params = "?lat="+latLon[0]+"&lon="+latLon[1]+"&max_distance=3000";
-  var uri = baseUri + params;
+  var params  = "?lat="+latLon[0]+"&lon="+latLon[1]+"&max_distance=3000";
+  var uri     = baseUri + params;
   return uri;
 }
 
@@ -30,13 +30,18 @@ function callApi() {
 /* Process request */
 function processRequest() {
   if (xhr.readyState == 4 && xhr.status == 200) {
-    var location = xhr.response.location;
-    var machineNames = location.machine_names;
-    if (machineNames.length > 0) {
+    var location      = xhr.response.location;
+    var machineNames  = location.machine_names;
+    var machineLength = machineNames.length;
+    if (machineLength > 0) {
       document.getElementById('location-name').innerHTML = location.name;
       document.getElementById('location-city').innerHTML = location.city + ", " + location.state;
-      document.getElementById('machine-count').innerHTML = machineNames.length + " machines";
-      for (var i = 0; i < machineNames.length; i++) {
+      var machineCount = machineLength + " machine";
+      if (machineLength > 1) {
+        machineCount += "s";
+      }
+      document.getElementById('machine-count').innerHTML = machineCount;
+      for (var i = 0; i < machineLength; i++) {
         var ul = document.getElementById('machine-list');
         var li = document.createElement('li');
         li.appendChild(document.createTextNode(machineNames[i]));
